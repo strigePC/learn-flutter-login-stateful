5b9f1c1b00ef1ae7274029f9f41f3e55c979299b
Anuar Otynshin <strigeandro@gmail.com>
2019-05-15 16:22:30 +0600

    customize keyboard type for email address

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -27,6 +27,7 @@ class LoginScreenState extends State<LoginScreen> {
 
   Widget emailField() {
     return TextFormField(
+      keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         labelText: 'Email Address',
         hintText: 'example@mail.com'
