5e6d1cc4f911b71969d078a97f9eeffe97051d4a
Anuar Otynshin <strigeandro@gmail.com>
2019-05-15 16:07:51 +0600

    create a login screen with container, add an empty form

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:login_stateful/src/screens/login_screen.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
     return MaterialApp(
       title: 'Log Me In!',
       home: Scaffold(
-        body: Text('Show a form here!'),
+        body: LoginScreen(),
       ),
     );
   }
